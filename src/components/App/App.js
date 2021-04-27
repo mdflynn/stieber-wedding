149bab7a71f7cdcd42a955adf75f9ee2f75d4413
Mike Flynn <mdflynn34@outlook.com>
2021-04-26 20:36:10 -0600

    add router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-<h1>Hello World</h1>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
