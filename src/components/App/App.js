49b1981a795b700482cefba13154572d270df7d3
Mike Flynn <mdflynn34@outlook.com>
2021-04-26 20:41:20 -0600

    Add node-sass

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./App.scss";
+import MainPage from "../MainPage/MainPage";
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Nav />
+      <MainPage />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
