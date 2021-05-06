9b9f4d84150049e942b15469fd052fb0c7328c9e
Mike Flynn <mdflynn34@outlook.com>
2021-05-05 18:08:43 -0600

    Add Rsvp to Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import MainPage from "../MainPage/MainPage";
 import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
 import Gallery from "../Gallery/Gallery";
+import Rsvp from "../Rsvp/Rsvp";
 import { Route, Switch } from "react-router-dom";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={MainPage} />
         <Route path="/gallery" component={Gallery} />
+        <Router path="/rsvp" component={Rsvp} />
       </Switch>
       <Footer />
     </>
