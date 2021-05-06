1371a18735314e772b9022023cc2541d7f31f088
Mike Flynn <64620860+mdflynn@users.noreply.github.com>
2021-05-05 19:36:26 -0600

    Feature/rsvp (#15)
    * initial commit
    
    * add rsvp initial setup
    
    * Add Rsvp to Router
    
    * correct route error
    
    * add template
    
    * add styling
    
    * remove undeed inputs, add textarea
    
    * add styling to text area
    
    * add name function
    
    * add guests function
    
    * add email function
    
    * initial commit
    
    * add styling to text
    
    * add message function
    
    * remove link decoration
    
    * add shimmer effect
    
    * change effect
    
    * add handleform structure

---
+++ src/components/Rsvp/Rsvp.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import "./Rsvp.scss";
+
+const Rsvp = () => {
+  const [attend, setAttend] = useState(null);
+  const [name, setName] = useState("");
+  const [guests, setGuests] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleGuestChange = (e) => {
+    setGuests(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+      setMessage(e.target.value);
+  }
+
+  const handleFormSubmit = (e) => {
+      e.preventDefault();
+      // add emailJS 
+      // incomplete form check
+  }
+
+  return (
+    <form>
+      <div className="segment">
+        <h1 className="shimmer">RSVP</h1>
+      </div>
+      <label className="form-label">
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label className="form-label">
+        <input
+          className="form-input"
+          type="number"
+          min={0}
+          placeholder="Number of Guests"
+          value={guests}
+          onChange={handleGuestChange}
+        />
+      </label>
+      <label className="form-label">
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Email Address"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </label>
+      <label className="form-label">
+        <textarea
+          rows="4"
+          cols="50"
+          placeholder="   Message (optional)"
+          value={message}
+          onChange={handleMessageChange}
+        ></textarea>
+      </label>
+      <button className="form-button" type="button">
+        <i className="icon"></i> Send
+      </button>
+    </form>
+  );
+};
+
+export default Rsvp;
