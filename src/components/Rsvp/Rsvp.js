eee720641007b28c9c4354e8f5ef6bbe6d6dcb91
Mike Flynn <mdflynn34@outlook.com>
2021-05-05 19:00:59 -0600

    add name function

--- src/components/Rsvp/Rsvp.js
+++ src/components/Rsvp/Rsvp.js
@@ -3,6 +3,12 @@ import "./Rsvp.scss";
 
 const Rsvp = () => {
   const [attend, setAttend] = useState(null);
+  const [name, setName] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
 
   return (
     <form>
@@ -10,16 +16,31 @@ const Rsvp = () => {
         <h1>RSVP</h1>
       </div>
       <label className="form-label">
-        <input className="form-input" type="text" placeholder="Name" />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={handleNameChange}
+        />
       </label>
       <label className="form-label">
-        <input className="form-input" type="number" min={0} placeholder="Number of Guests" />
+        <input
+          className="form-input"
+          type="number"
+          min={0}
+          placeholder="Number of Guests"
+        />
       </label>
       <label className="form-label">
         <input className="form-input" type="text" placeholder="Email Address" />
       </label>
       <label className="form-label">
-        <textarea rows="4" cols="50" placeholder="   Message (optional)"></textarea>
+        <textarea
+          rows="4"
+          cols="50"
+          placeholder="   Message (optional)"
+        ></textarea>
       </label>
       <button className="form-button" type="button">
         <i className="icon ion-md-lock"></i> Send
