43ed7d84f01369f542af8f860b97f02e198da810
Mike Flynn <mdflynn34@outlook.com>
2021-05-06 20:32:25 -0600

    add text placeholder

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -5,12 +5,14 @@ import stockOne from "../../assets/stock-1.jpg";
 const MainPage = () => {
   return (
     <main>
-      <div className="bg"></div>
-      <p>
-        This example creates a full page background image. Try to resize the
-        browser window to see how it always will cover the full screen (when
-        scrolled to top), and that it scales nicely on all screen sizes.
-      </p>
+      <div className="bg">
+        <div className="caption">
+          <span className="border">David</span>
+          <br />
+          <br />
+          <span className="border">& Abbey</span>
+        </div>
+      </div>
     </main>
   );
 };
