c4f64900473092e5f0e211eda6f647414ff40520
Mike Flynn <mdflynn34@outlook.com>
2021-05-10 15:31:21 -0600

    add external link to button

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./Location.scss";
 import ranch from "../../assets/ranch.jpeg";
 
 const Location = () => {
   return (
-    <div className="location-div">
-      <img className="location-img" src={ranch} alt="ranch" />
-      <span className="location-text">
-        September 25th <br />@<br />
-        Younger Ranch
-      </span>
-    </div>
+    <Fragment>
+      <div className="location-div">
+        <img className="location-img" src={ranch} alt="Younger Ranch" />
+        <span className="location-text">
+          September 25th <br />@<br />
+          Younger Ranch
+        </span>
+      </div>
+      <a
+        class="direction-btn"
+        href="https://www.google.com/maps/place/Younger+Ranch/@39.0918622,-104.7140757,12.25z/data=!4m5!3m4!1s0x0:0x850abcd72753b394!8m2!3d39.1032474!4d-104.7243749"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Get Directions
+      </a>
+    </Fragment>
   );
 };
 
