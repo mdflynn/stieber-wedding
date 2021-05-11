55f926ea52ce3996bc6c485ba916172b2d007c3f
Mike Flynn <mdflynn34@outlook.com>
2021-05-11 12:50:20 -0600

    add text and styling

--- src/components/Registry/Registry.js
+++ src/components/Registry/Registry.js
@@ -3,16 +3,22 @@ import "./Registry.scss";
 
 const Registry = () => {
   return (
-    <div className="container">
-      <a
-        href="https://paypal.me/pools/c/8zmuv6khmd"
-        target="_blank"
-      >
-        <div className="registry-btn">
-          <span>Donate</span>
+    <div className="registry-div">
+      <p>
+        The most important thing for us is your presence during the day to<br />
+        celebrate our marriage, but if you do want to make a gesture, please<br />
+        feel free to donate to our Honeymoon Pool below
+      </p>
+      <div>
+        <div className="container">
+          <a href="https://paypal.me/pools/c/8zmuv6khmd" target="_blank">
+            <div className="registry-btn">
+              <span>Donate</span>
+            </div>
+            <div className="sun"></div>
+          </a>
         </div>
-        <div className="sun"></div>
-      </a>
+      </div>
     </div>
   );
 };
