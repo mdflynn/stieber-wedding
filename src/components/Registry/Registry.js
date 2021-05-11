ebec8f88700cd753736931d66a8ab76c0297d90f
Mike Flynn <mdflynn34@outlook.com>
2021-05-11 12:10:08 -0600

    add sunset button

--- src/components/Registry/Registry.js
+++ src/components/Registry/Registry.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "./Registry.scss";
 
 const Registry = () => {
-    return (
-        <h1>Registry</h1>
-    )
-}
+  return (
+    <div className="container">
+      <a
+        href="https://paypal.me/pools/c/8zmuv6khmd"
+        target="_blank"
+      >
+        <div className="registry-btn">
+          <span>Donate</span>
+        </div>
+        <div className="sun"></div>
+      </a>
+    </div>
+  );
+};
 
 export default Registry;
