2bc52779732968780983fef5dae1d567e8871743
Mike Flynn <mdflynn34@outlook.com>
2021-05-25 16:12:31 -0600

    change nav to last name

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => {
   return (
     <nav>
       <Link to="/" id="nav-name">
-        <p className="nav-text">David & Abbey</p>
+        <p className="nav-text">The Stiebers</p>
       </Link>
       <NavLink to="/location" className="nav-link">
         <p className="nav-text">When & Where</p>
