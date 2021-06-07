1dfa9594157594df1dfc7eb10225010b144e0d2d
Mike Flynn <64620860+mdflynn@users.noreply.github.com>
2021-06-07 14:59:44 -0600

    Feature/accessibility (#37)
    * update text color for readability
    
    * update wedding time
    
    * initial img commit
    
    * import new pictures
    
    * update site description and title
    
    * remove stock photos and add new images
    
    * remove extra styling for accessibility
    
    * add key to mapped images
    
    * update class to className

--- src/assets/gallery-imgs/index.js
+++ src/assets/gallery-imgs/index.js
@@ -1,6 +1,87 @@
-import imgOne from "./hugging.jpg";
-import imgTwo from "./kissing.jpg";
+import imgOne from "./IMG_11.jpg";
+import imgTwo from "./IMG_12.jpg";
+import imgThree from "./IMG_13.jpg";
+import imgFour from "./IMG_14.jpg";
+import imgFive from "./IMG_15.jpg";
+import imgSix from "./IMG_16.jpg";
+import imgSeven from "./IMG_17.jpg";
+import imgEight from "./IMG_18.jpg";
+import imgNine from "./IMG_19.jpg";
+import imgTen from "./IMG_20.jpg";
+import imgEleven from "./IMG_21.jpg";
+import imgTwelve from "./IMG_22.jpg";
+import imgThirteen from "./IMG_23.jpg";
+import imgFourteen from "./IMG_24.jpg";
+import imgFifteen from "./IMG_25.jpg";
+import imgSixteen from "./IMG_26.jpg";
+import imgSeventeen from "./IMG_27.jpg";
+import imgEighteen from "./IMG_28.jpg";
+import imgNineteen from "./IMG_29.jpg";
+import imgTwenty from "./IMG_30.jpg";
+import imgTwentyOne from "./IMG_31.jpg";
+import imgTwentyTwo from "./IMG_32.jpg";
+import imgTwentyThree from "./IMG_33.jpg";
+import imgTwentyFour from "./IMG_34.jpg";
+import imgTwentyFive from "./IMG_35.jpg";
+import imgTwentySix from "./IMG_36.jpg";
+import imgTwentySeven from "./IMG_37.jpg";
+import imgTwentyEight from "./IMG_38.jpg";
+import imgTwentyNine from "./IMG_39.jpg";
+import imgThirty from "./IMG_40.jpg";
+import imgThirtyOne from "./IMG_41.jpg";
+import imgThirtyTwo from "./IMG_42.jpg";
+import imgThirtyThree from "./IMG_43.jpg";
+import imgThirtyFour from "./IMG_44.jpg";
+import imgThirtyFive from "./IMG_45.jpg";
+import imgThirtySix from "./IMG_46.jpg";
+import imgThirtySeven from "./IMG_47.jpg";
+import imgThirtyEight from "./IMG_48.jpg";
+import imgThirtyNine from "./IMG_49.jpg";
+import imgForty from "./IMG_50.jpg";
+import imgFortyOne from "./IMG_51.jpg";
+import imgFortyTwo from "./IMG_52.jpg";
 
 export const galleryImgs = [
-    imgOne, imgTwo
-]
+  imgOne,
+  imgTwo,
+  imgThree,
+  imgFour,
+  imgFive,
+  imgSix,
+  imgSeven,
+  imgEight,
+  imgNine,
+  imgTen,
+  imgEleven,
+  imgTwelve,
+  imgThirteen,
+  imgFourteen,
+  imgFifteen,
+  imgSixteen,
+  imgSeventeen,
+  imgEighteen,
+  imgNineteen,
+  imgTwenty,
+  imgTwentyOne,
+  imgTwentyTwo,
+  imgTwentyThree,
+  imgTwentyFour,
+  imgTwentyFive,
+  imgTwentySix,
+  imgTwentySeven,
+  imgTwentyEight,
+  imgTwentyNine,
+  imgThirty,
+  imgThirtyOne,
+  imgThirtyTwo,
+  imgThirtyThree,
+  imgThirtyFour,
+  imgThirtyFive,
+  imgThirtySix,
+  imgThirtySeven,
+  imgThirtyEight,
+  imgThirtyNine,
+  imgForty,
+  imgFortyOne,
+  imgFortyTwo,
+];
